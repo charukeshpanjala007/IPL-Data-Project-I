d04c3c667973021ede473eb469d728155e113164
charukeshpanjala007 <charukesh.panjala.19.1@mountblue.tech>
2022-03-24 15:53:25 +0530

    IPL Data Project I

--- src/server/problem6.js
+++ src/server/problem6.js
@@ -2,19 +2,46 @@ const fs = require('fs');
 const { matchData } = require("./ipl.js")
 
 //Problem6: Find a player who has won the highest number of Player of the Match awards for each season
-// const playerOfMatchDetailsOfEverySeason = matchData.reduce((result, currentValue) => {
-//     const { season, player_of_match } = currentValue
-//     if (result[season]) {
-//         if (result[season][player_of_match]) {
-//             result[season][player_of_match] += 1
-//         } else {
-//             result[season][player_of_match] = 1
-//         }
-//     } else {
-//         result[season] = {}
-//     }
-//     return result
-// }, {})
+const playerOfMatchDetailsOfEverySeason = matchData.reduce((result, currentValue) => {
+    const { season, player_of_match } = currentValue
+    if (result[season]) {
+        if (result[season][player_of_match]) {
+            result[season][player_of_match] += 1
+        } else {
+            result[season][player_of_match] = 1
+        }
+    } else {
+        result[season] = {}
+    }
+    return result
+}, {})
 
 
-// const highestNumberOfPlayerOfMatchForEachSeason = 
+const highestNumberOfPlayerOfMatchForEachSeason = Object.values(playerOfMatchDetailsOfEverySeason).reduce((result, currentValue) => {
+    let maxValue = Math.max(...Object.values(currentValue))
+    let [season] = Object.keys(playerOfMatchDetailsOfEverySeason).filter(eachItem => {
+        if(playerOfMatchDetailsOfEverySeason[eachItem] === currentValue){
+            return true
+        }
+    })
+    Object.keys(currentValue).forEach(eachItem=>{
+        if(currentValue[eachItem]=== maxValue){
+            if(result[season]){
+                result[season][eachItem] = maxValue
+            }else{
+                result[season]={}
+                result[season][eachItem] = maxValue
+             }
+        }
+    })
+    return result
+}, {})
+
+const jsonData = JSON.stringify(highestNumberOfPlayerOfMatchForEachSeason)
+fs.writeFile('src/public/output/highestNumberOfPlayerOfMatchForEachSeason.json', jsonData, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("success")
+    }
+})
